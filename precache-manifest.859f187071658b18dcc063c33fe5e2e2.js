89098bb4c9da6ceab2b923494a3e84d503d7617f
Your name hereSosen082 <sosnowski.przemek.95@gmail.com>
2020-02-25 16:48:56 +0100

    Updates

---
+++ precache-manifest.859f187071658b18dcc063c33fe5e2e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f7c0c8df3db2941002bdd21da56a066",
+    "url": "/notes-app/index.html"
+  },
+  {
+    "revision": "b135f2b3eda9d6f81b73",
+    "url": "/notes-app/static/css/main.b410f040.chunk.css"
+  },
+  {
+    "revision": "c3a7184bd1ad99a48499",
+    "url": "/notes-app/static/js/2.80f6fdb9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/notes-app/static/js/2.80f6fdb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b135f2b3eda9d6f81b73",
+    "url": "/notes-app/static/js/main.df330094.chunk.js"
+  },
+  {
+    "revision": "2130a92484021e424dbc",
+    "url": "/notes-app/static/js/runtime-main.88f094ce.js"
+  },
+  {
+    "revision": "9899b36a6b2175c45896ac7ffa283b41",
+    "url": "/notes-app/static/media/board.9899b36a.jpg"
+  }
+]);
