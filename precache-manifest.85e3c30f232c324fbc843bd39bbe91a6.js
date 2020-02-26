92319d129440d6b1c8be76d074fd63306d6b3541
Your name hereSosen082 <sosnowski.przemek.95@gmail.com>
2020-02-26 21:36:44 +0100

    Updates

---
+++ precache-manifest.85e3c30f232c324fbc843bd39bbe91a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1739e9318667fa9da9eb62aa7cd54c16",
+    "url": "/notes-app/index.html"
+  },
+  {
+    "revision": "199f0cbdcf029692bf66",
+    "url": "/notes-app/static/css/main.7052f025.chunk.css"
+  },
+  {
+    "revision": "cd5c6ff5bd08b5d28968",
+    "url": "/notes-app/static/js/2.642a10f1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/notes-app/static/js/2.642a10f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "199f0cbdcf029692bf66",
+    "url": "/notes-app/static/js/main.c7b1cbfd.chunk.js"
+  },
+  {
+    "revision": "2130a92484021e424dbc",
+    "url": "/notes-app/static/js/runtime-main.88f094ce.js"
+  },
+  {
+    "revision": "57cf6c5c7de1387a26e73467a47c1f62",
+    "url": "/notes-app/static/media/ReenieBeanie-Regular.57cf6c5c.ttf"
+  },
+  {
+    "revision": "9899b36a6b2175c45896ac7ffa283b41",
+    "url": "/notes-app/static/media/board.9899b36a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/notes-app/precache-manifest.859f187071658b18dcc063c33fe5e2e2.js"
+  "/notes-app/precache-manifest.85e3c30f232c324fbc843bd39bbe91a6.js"
 );
 
 self.addEventListener('message', (event) => {
